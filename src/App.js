03b3e43049010d64e516a4c4d93287eb9c7e1b8b
root <root@DESKTOP-JOHV3EV.localdomain>
2021-12-19 14:32:56 +0000

    fixing

--- src/App.js
+++ src/App.js
@@ -1,43 +1,17 @@
 import React, { useState, useEffect } from 'react'
+import Anecdote from './components/Anecdote'
+import Button from './components/Button'
+import Votebutton from './components/Votebutton'
+import DisplayMostVoted from './components/DisplayMostVoted'
 
-const Button = (props) => {
-  return(
-  <button onClick={props.onClick}>Next Anecdote</button>)
-} 
-
-const Anecdote = (props) => {
-  return (
-    <div>
-      <h1>Anecdote of the day</h1>
-      <p>{props.anecdote}</p>
-      <p>has {props.votes } votes</p>
-    </div>
-  )
-}
-
-const Votebutton = (props) => {
-  return (
-    <button onClick={props.onClick}>Vote for anecdote</button>
-  )
-}
-
-const DisplayMostVoted = (props) => {
-  return(
-    <div>
-      <h1>Anecdote with most votes</h1>
-      <p>{props.votes}</p>
-      <p>has {props.voted} votes</p>
-    </div>
-  )
-}
 
 const App = () => {
   const anecdotes = [
-    'If it hurts, do it more often',
+    'Premature optimization is the root of all evil.',
     'Adding manpower to a late software project makes it later!',
     'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
     'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
+    'If it hurts, do it more often.',
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
   ]
